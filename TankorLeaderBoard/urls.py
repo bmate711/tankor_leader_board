13d95829e1b98a4fcbc17eaf4da6df085cdd2dc7
Bodor Máté <bodor.mate@kszk.bme.hu>
2018-11-12 23:35:31 +0100

    fixed

--- TankorLeaderBoard/urls.py
+++ TankorLeaderBoard/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('^api/v1/', include('leaderBoard.urls')),
+    path('api/v1/', include('leaderBoard.urls')),
     path('admin/', admin.site.urls),
 ]
